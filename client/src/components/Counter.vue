2954a7205f3e8d95f5b2a2f08a43b2177035e9cc
aldymartinus <48199683+aldymartinus@users.noreply.github.com>
2021-05-16 15:02:26 +0700

    fixed undercounting issue

--- client/src/components/Counter.vue
+++ client/src/components/Counter.vue
@@ -1,46 +1,53 @@
 <template>
-    <div class="counter">
-        <button @click="minusCount">-</button>
-        <input v-model="quantity" type="text" readonly inputmode="numeric" aria-valuemin="0">
-        <button @click="plusCount">+</button>
-    </div>
+  <div class="counter">
+    <button :disabled="(quantity < 1) ? true : false" @click="minusCount">-</button>
+    <input
+      v-model="quantity"
+      type="text"
+      readonly
+      inputmode="numeric"
+      aria-valuemin="0"
+    />
+    <button @click="plusCount">+</button>
+  </div>
 </template>
 <script>
 export default {
-    name:'Counter',
-    data(){
-        return{
-            quantity:0
-        }
+  name: "Counter",
+  data() {
+    return {
+      quantity: 0,
+      isDisabled: true,
+    };
+  },
+  methods: {
+    plusCount() {
+      this.quantity += 1;
+      this.$emit("clicked", this.quantity);
     },
-    methods:{
-        plusCount(){
-            this.quantity += 1
-            this.$emit('clicked',this.quantity);
-        },
-        minusCount(){
-            if (this.quantity !== 0) {
-                this.quantity -= 1
-                this.$emit('clicked',this.quantity);
-            }
-        }
-    }
-}
+    minusCount() {
+      if (this.quantity !== 0) {
+        this.quantity -= 1;
+        this.$emit("clicked", this.quantity);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .counter{
-        display: flex;
-        flex-direction: row;
-    }
-    .counter input{
-        width: 2rem;
-        text-align: center;
-        border: none;
-        font-size: large;
-    }
-    .counter button{
-        font-weight: bold;
-        height: 1.8rem;
-    }
+.counter {
+  display: flex;
+  flex-direction: row;
+}
+.counter input {
+  width: 2rem;
+  text-align: center;
+  border: none;
+  font-size: large;
+}
+.counter button {
+  font-weight: bold;
+  height: 1.8rem;
+}
 </style>
